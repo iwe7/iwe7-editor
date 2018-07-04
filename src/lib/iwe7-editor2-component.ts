754c61eb0e7fa3c8c9447b758c711f2b73fe385f
iwe7 <admin@meepo.com.cn>
2018-07-04 18:37:27 +0800

    u

--- src/lib/iwe7-editor2-component.ts
+++ src/lib/iwe7-editor2-component.ts
@@ -1,4 +1,13 @@
+import { SFSchema, SFUISchema } from '@delon/form';
 export abstract class Iwe7Editor2Component<T> {
     public value: T;
+    schema: SFSchema = {
+        properties: {}
+    };
+    ui: SFUISchema = {};
+    default: T;
     constructor() { }
+    formChange(val: T) {
+        this.value = val;
+    }
 }
